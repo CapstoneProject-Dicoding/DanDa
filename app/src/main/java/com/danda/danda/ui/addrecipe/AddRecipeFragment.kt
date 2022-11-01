172ce5c972f8041a497a1cd799126d2c7560ab9c
Waslim <waslim19240@gmail.com>
2022-11-01 21:11:38 +0700

    Adding Bottom Navigation

--- app/src/main/java/com/danda/danda/ui/addrecipe/AddRecipeFragment.kt
+++ app/src/main/java/com/danda/danda/ui/addrecipe/AddRecipeFragment.kt
@@ -1,4 +1,4 @@
-package com.danda.danda.ui.notifications
+package com.danda.danda.ui.addrecipe
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,14 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.danda.danda.databinding.FragmentNotificationsBinding
+import com.danda.danda.databinding.FragmentAddRecipeBinding
 
-class NotificationsFragment : Fragment() {
+class AddRecipeFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentAddRecipeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,14 +19,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val addRecipeViewModel =
+            ViewModelProvider(this).get(AddRecipeViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentAddRecipeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        addRecipeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/danda/danda/ui/addrecipe/AddRecipeViewModel.kt
+++ app/src/main/java/com/danda/danda/ui/addrecipe/AddRecipeViewModel.kt
@@ -1,10 +1,10 @@
-package com.danda.danda.ui.dashboard
+package com.danda.danda.ui.addrecipe
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class AddRecipeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
