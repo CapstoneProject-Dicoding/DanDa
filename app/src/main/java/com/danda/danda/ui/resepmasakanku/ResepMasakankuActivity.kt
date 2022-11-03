55552cb6df54c3d49f36557446fa469ad5520cea
noorchalim <noorchalim3@gmail.com>
2022-11-03 21:23:50 +0700

    Membuat layout add recipe, membuat layout masakanku

---
+++ app/src/main/java/com/danda/danda/ui/resepmasakanku/ResepMasakankuActivity.kt
@@ -0,0 +1,12 @@
+package com.danda.danda.ui.resepmasakanku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.danda.danda.R
+
+class ResepMasakankuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resep_masakanku)
+    }
+}
