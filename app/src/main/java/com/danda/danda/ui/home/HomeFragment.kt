30cf37d1a39a783139f6de46e012b4ff62672924
Waslim <waslim19240@gmail.com>
2022-11-13 16:08:46 +0700

    image slider with firebase

--- app/src/main/java/com/danda/danda/ui/home/HomeFragment.kt
+++ app/src/main/java/com/danda/danda/ui/home/HomeFragment.kt
@@ -17,12 +17,9 @@ import java.util.ArrayList
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private val homeViewModel by viewModels<HomeViewModel>()
-
-    // firestore
     private lateinit var firestore: FirebaseFirestore
 
     override fun onCreateView(
@@ -30,15 +27,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-//        val homeViewModel =
-//            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
         return binding.root
 
 
@@ -55,28 +44,8 @@ class HomeFragment : Fragment() {
             binding.textHome.text = "Anda belum login"
         }
 
-//        apiViewModel.getSellerBanner().observe(viewLifecycleOwner) {
-//            when (it.status) {
-//                SUCCESS -> {
-//                    val data = it.data!!
-//                    val list = ArrayList<SlideModel>()
-//
-//                    for (img in data) {
-//                        list.add(SlideModel(img.imageUrl, ScaleTypes.FIT))
-//                    }
-//
-//                    binding.imgSlider.setImageList(list)
-//                }
-//                ERROR -> {
-//
-//                }
-//                LOADING -> {
-//
-//                }
-//            }
-//        }
 
-
+        getBanner()
 
 //        homeViewModel.getBanner()
 //        homeViewModel.banner.observe(viewLifecycleOwner) { status ->
@@ -95,9 +64,13 @@ class HomeFragment : Fragment() {
 //            }
 //        }
 
+    }
+
+    private fun getBanner() {
+        firestore = FirebaseFirestore.getInstance()
+
         val imageList = ArrayList<SlideModel>()
 
-        firestore = FirebaseFirestore.getInstance()
         firestore.collection("image_slider")
             .get()
             .addOnSuccessListener {
@@ -109,31 +82,8 @@ class HomeFragment : Fragment() {
                 }
             }
             .addOnFailureListener {
-
+                //
             }
-
-//        firestore.collection("image_slider").get()
-//            .addOnCompleteListener {
-//                if (it.isSuccessful) {
-//                    for ()
-//                }
-//            }
-//
-//            .addOnFailureListener {
-//
-//            }
-
-
-//        val imageList = ArrayList<SlideModel>()
-
-//        imageList.add(SlideModel("https://asset-a.grid.id/crop/0x0:0x0/x/photo/2020/01/30/1299509524.jpg", ScaleTypes.FIT))
-//        imageList.add(SlideModel("https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2021/10/23/975567698.jpg", ScaleTypes.FIT))
-//        imageList.add(SlideModel("https://i3.wp.com/any.web.id/wp-content/uploads/2017/05/Pegunungan.jpg", ScaleTypes.FIT))
-//        imageList.add(SlideModel("https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1631074757/photo-1530878902700-5ad4f9e4c318_wqnmpp.jpg", ScaleTypes.FIT))
-//
-//        binding.imageSliderHome.setImageList(
-//            imageList
-//        )
     }
 
     override fun onDestroyView() {
