58b3e4324a58a5b1aabf008bf72d63aff0b643ea
Faldi PC <darkfaldi@gmail.com>
2022-11-25 21:37:38 +0700

    update get user profile(2)

--- app/src/main/java/com/danda/danda/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/danda/danda/ui/profile/ProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -15,6 +16,7 @@ import com.danda.danda.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 import com.danda.danda.util.Result
+import com.danda.danda.util.showToast
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment() {
@@ -54,7 +56,7 @@ class ProfileFragment : Fragment() {
                 }is Result.Failure ->{
                 binding.profileUsernameTv.text = user.error.toString()
                 }else->{
-
+                    requireActivity().showToast("bang jago")
                 }
             }
 
